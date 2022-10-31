2784c39f25ab3babd5125f4e17307d019baf43b1
Kyle Flynn <kyle.flynn5798@yahoo.com>
2022-10-31 19:56:56 -0400

    small mods

--- front-end/src/screens/Stream/GameOverview/GameOverview.tsx
+++ front-end/src/screens/Stream/GameOverview/GameOverview.tsx
@@ -1,5 +1,14 @@
 import { FC } from "react";
+import PlayerView from "./components/PlayerView";
+import classes from './GameOverview.module.less';
 
 export const GameOverview: FC = () => {
-  return <div>game overview</div>;
+  return (
+    <div className={classes.playerContainer}>
+      <PlayerView />
+      <PlayerView />
+      <PlayerView />
+      <PlayerView />
+    </div>
+  );
 };
---
+++ front-end/src/screens/Stream/GameOverview/components/PlayerView.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+const PlayerView: FC = () => {
+    return (
+        <div>Testing</div>
+    );
+};
+
+export default PlayerView;
