d60cb1da16af330bfc7e828c7ed1f2a687ea58e5
Kyle Flynn <kyleflynn.dev@gmail.com>
2022-11-02 13:04:27 -0400

    Starting work on admin panel

--- front-end/src/screens/Admin/AdminPanel.tsx
+++ front-end/src/screens/Admin/AdminPanel.tsx
@@ -1,5 +1,12 @@
 import { FC } from "react";
+import classes from "./AdminPanel.module.less";
 
 export const AdminPanel: FC = () => {
-  return <div>admin panel</div>;
+  return (
+    <div className={`container ${classes.adminPanel}`}>
+      <div>
+        <h1>Mario Party Admin Panel</h1>
+      </div>
+    </div>
+  );
 };
